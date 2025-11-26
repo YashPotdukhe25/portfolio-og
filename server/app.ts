112860ed76371c1e5f04436427592dddcc6f0f4b
Necromancercorroder <traitor659@gmail.com>
2025-11-26 19:25:49 +0530

    Initial commit with fixes for Windows compatibility

--- server/app.ts
+++ server/app.ts
@@ -82,8 +82,8 @@ export default async function runApp(
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: process.platform === 'win32' ? 'localhost' : '0.0.0.0',
+    reusePort: process.platform !== 'win32',
   }, () => {
     log(`serving on port ${port}`);
   });
